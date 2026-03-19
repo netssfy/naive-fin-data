935af76aa432a178cfafa2654815e5e3786b2ce3
netssfy <netssfy@126.com>
2026-03-19 23:42:21 +0800

    feat(trader-incubator): add simulated exchange runtime and strategy API

---
+++ src/trader_incubator/__init__.py
@@ -0,0 +1,10 @@
+from trader_incubator.exchange import Exchange, Order, SymbolRef, TradingSessionConfig, TradingStrategy
+
+__all__ = [
+    "Exchange",
+    "Order",
+    "SymbolRef",
+    "TradingSessionConfig",
+    "TradingStrategy",
+]
+
